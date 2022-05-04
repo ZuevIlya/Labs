5abb13b454cfca6d85da9085ba3a0d1d961783a6
ZuevIlya <dartveyder161@mail.ru>
2022-05-04 15:49:51 +0300

    lab3

--- FirstStep/FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
+++ FirstStep/FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
@@ -1,38 +1,88 @@
 package com.example.firststep;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.firststep.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+
+public class MainActivity extends AppCompatActivity implements TransactionEvents{
+    ActivityResultLauncher activityResultLauncher;
+        private String pin;
 
     // Used to load the 'firststep' library on application startup.
     static {
         System.loadLibrary("first-step");
         System.loadLibrary("mbedcrypto");
     }
-
     private ActivityMainBinding binding;
+        @Override
+        public String enterPin(int ptc, String amount) {
+            pin = new String();
+            Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+            it.putExtra("ptc", ptc);
+            it.putExtra("amount", amount);
+            synchronized (MainActivity.this) {
+                activityResultLauncher.launch(it);
+                try {
+                    MainActivity.this.wait();
+                } catch (Exception ex) {
+                    //todo: log error
+                }
+            }
+            return pin;
+        }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
+                    }
+                });
         int res = initRng();
         byte[] rnd = randomBytes(10);
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-
         TextView test = findViewById(R.id.rnd_elem);
-        test.setText("Лаба №1");
+        test.setText("Лаба №3");
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
     }
 
 
@@ -41,4 +91,25 @@ public class MainActivity extends AppCompatActivity {
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
+    public native boolean transaction(byte[] trd);
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+    public void onButtonClick(View v)
+    {
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
+    }
+
 }
